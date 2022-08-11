77f3c5f727b5a1c72d3ecbbfe7a0517390f42c13
David Healy <drtjprdave@gmail.com>
2022-08-11 15:53:32 -0600

    Working on getting new paragraphs to show.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# web-apis-challenge
+## Description
 
-Web APIS Challenge
+This is a quiz quiz with a timer. Time is docked for missed answers. You start the quiz by clicking on LET'S GO and then answer the questions presented.
+
+- I wanted wanted the user to start the quiz and answer the questions. At the end you would see your score and be able to put your initials in. You can also restart the quiz and log new scores. You can also reload the app.
+- This project pushed my initial limits as a coder and learning through this process will give me the skill needed to continue my career in coding.
+- It makes navigation of the page simple and lets you know exactly how to use the application.
+- I learned how to be able to add a timer and also taught me how to cycle through other questions.
+
+## URL Directory
+
+## Usage
--- develop/js/script.js
+++ develop/js/script.js
@@ -1,97 +1,61 @@
 var timeLeft = 5;
     var element = document.querySelector(".timeLeft");
     var secLeft = " seconds left.";
-    var nextPage = document.querySelector(".timerStart");
+    var letsGo = document.querySelector(".timerStart");
+    
+var questions = [
+    {
+        title: "What is the Snickers Bar original name?",
+        correctAnswer: "Marathon Bar",
+        choices: [
+            "Wonka Bar",
+            "Chunky Bar"
+        ]
+    },
+    {
+        title: "What was the original name for cotton candy?",
+        correctAnswer: "Fairy Floss",
+        choices: [
+            "Fairy Floss",
+            "Candu Spider Web",
+            "Dental Damage"
+        ]
+    },
+]
+console.log(questions[0].title);
 
+
+var introParagraph = document.querySelector(".intro");
+var letsGo = document.querySelector(".timerStart");
+var currentIndex = 0;
+var  pEl = document.getElementById("q2");
+// console.log(pEl);
+
+function startQuiz(event) {
     
+    letsGo.setAttribute("style", "display: none");
+    introParagraph.setAttribute("style", "display: none");
     
-    function nextPage001(event) {
-        console.log(nextPage);
-        
-        var timerId = setInterval(countdown, 1000);
-        function countdown() {
-            if (timeLeft === 0) {
-                alert("Times Up")
-                clearInterval(timerId);
+
+    pEl.textContent = questions[0].title;
+    //TODO: How can I present the choices to the user?
+
+    console.log(letsGo);
+    
+    var timerId = setInterval(countdown, 1000);
+    function countdown() {
+        if (timeLeft === 0) {
+            alert("Times Up");
+            clearInterval(timerId);
                 return;
             } else {
                 timeLeft--;
             }
             console.log(timeLeft);
             element.textContent = timeLeft + " seconds left.";
-            }
+        }
     }
     
-    nextPage.addEventListener("click", nextPage001);
-
-// function genQuiz(questions, quizContainer, resultContainer, submitButton) {
-
-//     function showQuestions(questions, quizContainer); {
-
-//     }
-
-//     function showResults(questions, quizContainer, resultContainer); {
-
-//     }
-
-//     showQuestions(questions, quizContainer); {
-
-//     }
-
-//     submitButton.timerStart = function() {
-//         showResults(showResults, quizContainer, resultContainer);
-//     }
-
-// }
-
-var questions = [
-    {
-        questions: "How many licks does it take to get to the Tootsie Roll center of a Tootsie Pop?",
-
-        answer: {
-            a: "3",
-            b: "7",
-            c: "1,395,209"
-        },
-        {
-        question: "What was the original name of the Snickers Bar?",
-            a: "Marathon",
-            b: "Horse Bar",
-            c: "Wonka Bar"
-        },
-        {
-        question: "What was the original name of cotton candy?",
-            a: "Dental Damage",
-            b: "Fairy Floss",
-            c: "Candy Spider Web"
-        },       
-    }
-];
-
-function showQuestions(questions, quizContainer) {
-    var outPut = [];
-    var answer;
-
-    for (var i=0; 1<questions.length; i++) {
-
-        answer = [];
-
-        for(letter in question[i].answer) {
-            answer.push(
-                "<label>"
-                    + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                    + letter + ': '
-                    + question[i].answer[letter]
-                + '</label>'
-            );
-        }
-
-        outPut.push(
-            '<div class="answer">' + question[i].question + '</div>'
-        );
-    }
-
-    questionContainer.innerHtml = outPut.join("");
-}
-
-showQuestions(questions, quizContainer);
+    letsGo.addEventListener("click", startQuiz);
+    
+    
