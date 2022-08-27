a3927b7bc4524c23e3d1ed8bed7c44a12722c1ed
David Healy <drtjprdave@gmail.com>
2022-08-27 14:17:16 -0600

    done for now.

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,17 +1,13 @@
-var timeLeft = 5;
+var timeLeft = 10;
 var element = document.querySelector(".timeLeft");
 var secLeft = " seconds left.";
 var letsGo = document.querySelector(".buttons");
 var introParagraph = document.querySelector(".intro");
 var currentIndex = 0;
-var p1El = document.getElementById("q1");
-var p2El = document.getElementById("q2");
 var questionContainer = document.querySelector(".questionContainer");
 var score = 0;
-var stopQuizContainer = $(".stopQuiz");
-
-// var quizEnd =
-
+var stopQuizContainer = document.querySelector(".stopQuiz");
+var savedScores = JSON.parse(localStorage.getItem("score")) || [];
 var questions = [
   {
     title: "What is the Snickers Bar original name?",
@@ -24,36 +20,33 @@ var questions = [
     choices: ["Fairy Floss", "Candy Spider Web", "Dental Damage"],
   },
 ];
-
 function startQuizButton() {
   letsGo.setAttribute("style", "display: none");
   introParagraph.setAttribute("style", "display: none");
-
   // console.log(letsGo);
   //This sets the timer off when you click let's go. It also displays the timer on the page.
   var timerId = setInterval(countdown, 1000);
   function countdown() {
     if (timeLeft === 0) {
-      alert("Times Up");
+      // alert("Times Up");
       clearInterval(timerId);
       return;
+    } else if (currentIndex === questions.length) {
+      clearInterval(timerId);
+      element.innerHTML = "";
     } else {
       timeLeft--;
+      element.textContent = timeLeft + " seconds left.";
     }
     // console.log(timeLeft);
-    element.textContent = timeLeft + " seconds left.";
   }
   nextQuestion();
 }
-
 letsGo.addEventListener("click", startQuizButton);
-
 function nextQuestion() {
   // p1El.innerHTML = "";
   letsGo.innerHTML = "";
-
   questionContainer.innerHTML = "";
-
   var currentQuestion = questions[currentIndex];
   questionContainer.textContent = currentQuestion.title;
   console.log(currentQuestion);
@@ -64,49 +57,51 @@ function nextQuestion() {
     choiceButton.setAttribute("class", "buttons");
     choiceButton.textContent = choiceOption;
     questionContainer.appendChild(choiceButton);
-    choiceButton.addEventListener("click", checkAnswer(currentQuestion));
+    // choiceButton.onclick = checkAnswer
+    choiceButton.addEventListener("click", checkAnswer);
   }
-
-  console.log(currentIndex);
-  // if (this.value === currentQuestion.correctAnswer) {
-  //     score += 2;
-  //     console.log("score; ", score );
-  //     currentIndex++;
-  //     nextQuestion();
-  // }
-  // if(this.value !== currentQuestion.correctAnswer) {
-  //     timeLeft -= 2
-  //     currentIndex++;
-  //     nextQuestion()
-  // }
-  // if ( currentIndex < currentQuestion.length) {
-  //  console.log("butts");
-  //     nextQuestion();
-  // }
-  // else if (
-  //     currentIndex === currentQuestion.length || timeLeft <= 1
-  // )
-  //     {
-  //     console.log("Times Up")
-  //     quizEnd();
-  // }
+  console.log("nextQuestion currentIndex: ", currentIndex);
 }
-
-function checkAnswer(currentQuestion) {
-  if (this.value === currentQuestion[currentIndex].correctAnswer) {
+console.log(questions.length);
+function checkAnswer() {
+  // check if the answer is correct, and give or retract numbers based on that
+  if (this.value === questions[currentIndex].correctAnswer) {
     score += 2;
     console.log("score; ", score);
   } else {
     timeLeft -= 2;
-    // currentIndex++;
-    // nextQuestion()
   }
-
+  // add one to the current index
   currentIndex++;
-  // nextQuestion();
+  // if cI is the same as the end of the question length, end quiz, otherwise next quest
+  if (currentIndex === questions.length) {
+    quizEnd();
+  } else {
+    nextQuestion();
+  }
 }
-
 function quizEnd() {
-  var apple = $("<h1> Your Score</h1>").hidden;
-  stopQuizContainer.append(apple);
+  questionContainer.innerHTML = "";
+  var apple = document.createElement("h1");
+  var banana = (score += timeLeft);
+  var orange = window.prompt("Enter your initials!!!");
+  var fruitBasket = document.createElement("h2");
+  fruitBasket.textContent = banana;
+  var scoreBoard = {
+    banana,
+    orange,
+  };
+  apple.textContent = "YOUR SCORE";
+  savedScores.push(scoreBoard);
+  localStorage.setItem("score", JSON.stringify(savedScores));
+
+  stopQuizContainer.appendChild(fruitBasket);
+  console.log(scoreBoard);
+  stopQuizContainer.appendChild(apple);
+  for (i = 0; i < savedScores.length; i++) {
+    var visibleScoreBoard = document.createElement("h3");
+    visibleScoreBoard.textContent =
+      savedScores[i].banana + "--" + savedScores[i].orange;
+    stopQuizContainer.appendChild(visibleScoreBoard);
+  }
 }
